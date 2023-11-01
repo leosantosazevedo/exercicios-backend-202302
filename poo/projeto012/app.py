c9d4c40b3b0f8d8d2451f770d209b906f712267e
Leonardo Azevedo <leosantosazevedo@gmail.com>
2023-10-31 21:43:40 -0300

    Adicionado rotas para tabelas Countries e CountryLanguage

--- poo/projeto012/app.py
+++ poo/projeto012/app.py
@@ -1,9 +1,46 @@
 import flask
-app = flask.Flask (__name__)
+import requests
+import json2table
 
+app = flask.Flask(__name__)
 @app.route("/")
-def OlaMundo():
-    return "<p>Ola Mundo </p>"
 
-if __name__ =='__main__':
+def OlaMundo():
+        return "<p>Ola Mundo</p>"
+
+@app.route("/consultaContinentes")
+def consultaContinentes():
+        response2 = requests.get("http://127.0.0.1:8000/getContinents")
+        json_object = response2.json()
+        build_direction = "LEFT_TO_RIGHT"
+        table_attributes = {"style" : "width:100%", "border": "1px solid black"}
+        html = json2table.convert(json_object, build_direction=build_direction, table_attributes=table_attributes)
+        #print(html)
+        #return response2.json()
+        return html
+
+@app.route("/consultaRegioes")
+def consultaRegioes():
+        response2 = requests.get("http://127.0.0.1:8000/getRegions")
+        json_object = response2.json()
+        build_direction = "LEFT_TO_RIGHT"
+        table_attributes = {"style" : "width:100%", "border": "1px solid black"}
+        html = json2table.convert(json_object, build_direction=build_direction, table_attributes=table_attributes)
+        #print(html)
+        #return response2.json()
+        return html
+
+@app.route("/consultaCountries")
+def consultaCountries():
+        response2 = requests.get("http://127.0.0.1:8000/getCountries")
+        json_object = response2.json()
+        build_direction = "LEFT_TO_RIGHT"
+        table_attributes = {"style" : "width:100%", "border": "1px solid black"}
+        html = json2table.convert(json_object, build_direction=build_direction, table_attributes=table_attributes)
+        #print(html)
+        #return response2.json()
+        return html
+
+
+if __name__ == '__main__':
     app.run(debug=True)
--- poo/projeto012/main.py
+++ poo/projeto012/main.py
@@ -27,6 +27,86 @@ def getContinent(continent_id : int):
     mycursor.close()
     return {"Continent": Continents_list}
 
+@app.get("/getCountries")
+def getCountries():
+    Countries_list = []
+    mycursor = connection.mydb.cursor(dictionary=True)
+    sql = "select * from countries"
+    mycursor.execute(sql)
+    for data_countries in mycursor:
+        Countries_list.append( data_countries )
+    mycursor.close()
+    return {"countries": Countries_list}
+
+@app.get("/getCountries/{country_id}")
+def getCountries(country_id : int):
+    Countries_list = []
+    mycursor = connection.mydb.cursor(dictionary=True)
+    sql="select * from countries where country_id = {0}".format(country_id)
+    mycursor.execute(sql)
+    for data_countries in mycursor:
+        Countries_list.append( data_countries )
+    mycursor.close()
+    return {"Countries": Countries_list}
+
+@app.get("/getCountryLanguage")
+def getCountryLanguage():
+    CountryLanguage_list = []
+    mycursor = connection.mydb.cursor(dictionary=True)
+    sql = "select * from country_languages"
+    mycursor.execute(sql)
+    for data_countrylanguage in mycursor:
+        CountryLanguage_list.append( data_countrylanguage )
+    mycursor.close()
+    return {"countrylanguage": CountryLanguage_list}
+
+@app.get("/getCountryLanguage/{language_id}")
+def getCountryLanguage(language_id : int):
+    CountryLanguage_list = []
+    mycursor = connection.mydb.cursor(dictionary=True)
+    sql="select * from country_languages where language_id = {0}".format(language_id)
+    mycursor.execute(sql)
+    for data_countrylanguage in mycursor:
+        CountryLanguage_list.append( data_countrylanguage )
+    mycursor.close()
+    return {"Continent": CountryLanguage_list}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @app.get("/getRegions")
 def getRegions():
     Regions_list = []
@@ -48,3 +128,14 @@ def getRegion(continent_id : int):
         Regions_list.append( data_regions )
     mycursor.close()
     return {"Region": Regions_list}
+
+#Refazer!!!
+@app.post('/createContinent/{continent_name}')
+def createContinent(continent_name: str):
+    if continent_name == "":
+        return {'Error': 'Item vazio'}
+    mycursor = connection.mydb.cursor(dictionary=True)
+    sql=" INSERT INTO continents (name) values ('{0}')".format(continent_name)
+    mycursor.execute(sql)
+    mycursor.close()
+    return {"Continent: OK"}
